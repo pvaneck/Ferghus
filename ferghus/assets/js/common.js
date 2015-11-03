9a51e0a6863b018b4a38455db8d6928501423680
Christopher Wright <cwright814@gmail.com>
2015-11-02 22:01:28 -0500

    BG converted to background-image; uses page-specific css

--- ferghus/assets/js/common.js
+++ ferghus/assets/js/common.js
@@ -1,6 +1,6 @@
 function bgSway() {
-	var bg = $("img.bg");
-	TweenMax.to(bg, 12, {x:64, ease:Sine.easeInOut});
-	TweenMax.to(bg, 12, {x:-64, ease:Sine.easeInOut, delay: 12, onComplete:bgSway});
+	var bg = $(".bg");
+	TweenMax.to(bg, 12, {x:48, ease:Sine.easeInOut});
+	TweenMax.to(bg, 12, {x:-48, ease:Sine.easeInOut, delay: 12, onComplete:bgSway});
 }
 bgSway();
