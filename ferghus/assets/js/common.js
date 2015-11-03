beb1bb1931820feb6205e4342e59315d1c95975e
Christopher Wright <cwright814@gmail.com>
2015-11-03 03:31:27 -0500

    Graphics and layout changes

--- ferghus/assets/js/common.js
+++ ferghus/assets/js/common.js
@@ -1,5 +1,5 @@
 function bgSway() {
-	var bg = $(".bg");
+	var bg = $("#bg");
 	TweenMax.to(bg, 12, {x:48, ease:Sine.easeInOut});
 	TweenMax.to(bg, 12, {x:-48, ease:Sine.easeInOut, delay: 12, onComplete:bgSway});
 }
