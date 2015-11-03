e5f22c6e72c4e2e66c63423d8a17db30e7dfb847
Christopher Wright <cwright814@gmail.com>
2015-11-03 16:47:13 -0500

    Improvements and cleanup for BG

--- ferghus/assets/js/common.js
+++ ferghus/assets/js/common.js
@@ -1,8 +1,8 @@
 // Endless sway ease for BG
 function bgSway() {
 	var bg = $("#bg");
-	TweenMax.to(bg, 12, {x:48, ease:Sine.easeInOut});
-	TweenMax.to(bg, 12, {x:-48, ease:Sine.easeInOut, delay: 12, onComplete:bgSway});
+	TweenMax.to(bg, 12, {x:64, ease:Sine.easeInOut});
+	TweenMax.to(bg, 12, {x:-64, ease:Sine.easeInOut, delay: 12, onComplete:bgSway});
 }
 bgSway();
 
