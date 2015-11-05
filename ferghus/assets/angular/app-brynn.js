c60675b6eef667b3fec7ba5b7958fca1183a6e1f
Christopher Wright <cwright814@gmail.com>
2015-11-05 00:37:48 -0500

    Minor cleanup

--- ferghus/assets/angular/app-brynn.js
+++ ferghus/assets/angular/app-brynn.js
@@ -4,62 +4,63 @@
 
 	app.controller('EnchantController', ['$scope', function($scope) {
 		var scope = $scope;
-		this.magic = 0;
-		this.successStart = 25;
-		this.successEnd = 40;
-		this.success = this.successStart;
-		this.selectedElixir = null;
-		scope.brEmpty = 100;
-		scope.brFull = 0;
+		var ctrl = this;
+		ctrl.magic = 0;
+		ctrl.successStart = 25;
+		ctrl.successEnd = 40;
+		ctrl.success = ctrl.successStart;
+		ctrl.selectedElixir = null;
+		ctrl.brEmpty = 100;
+		ctrl.brFull = 0;
 		var fill = $("#fill");
 		var brEmptyText = $("#brEmpty");
 		var brFullText = $("#brFull");
 		var brValues = {empty:100, full:0};
 
-		this.calcSuccess = function() {
-			if (this.magic <= 100)
-				this.success = Math.floor(this.successStart + (this.successEnd - this.successStart) * (this.magic / 100.0));
+		ctrl.calcSuccess = function() {
+			if (ctrl.magic <= 100)
+				ctrl.success = Math.floor(ctrl.successStart + (ctrl.successEnd - ctrl.successStart) * (ctrl.magic / 100.0));
 			else
-				this.success = this.successStart;
+				ctrl.success = ctrl.successStart;
 		}
 
-		this.updateBrText = function() {
+		ctrl.updateBrText = function() {
 			if (brValues.empty < 41)
 				brEmptyText.css("display", "none");
 			if (brValues.full >= 41)
 				brFullText.css("display", "initial");
 			scope.$apply(function() {
-				scope.brEmpty = Math.floor(brValues.empty);
-				scope.brFull = Math.ceil(brValues.full);
+				ctrl.brEmpty = Math.floor(brValues.empty);
+				ctrl.brFull = Math.ceil(brValues.full);
 			});
 		}
 
-		this.addMagic = function(br) {
-			this.magic = this.magic + br;
-			this.calcSuccess();
+		ctrl.addMagic = function(br) {
+			ctrl.magic = ctrl.magic + br;
+			ctrl.calcSuccess();
 			TweenMax.to(fill, 0.5, {
-				top:Math.max(143 - 143 * (this.magic / 100), 0),
+				top:Math.max(143 - 143 * (ctrl.magic / 100), 0),
 				ease:Power4.easeOut
 			});
 			TweenMax.to(brEmptyText, 0.5, {
-				top:Math.max(217 - 74 * (this.magic / 100), 0),
+				top:Math.max(217 - 74 * (ctrl.magic / 100), 0),
 				ease:Power4.easeOut
 			});
 			TweenMax.to(brFullText, 0.5, {
-				top:Math.max(291 - 74 * Math.min(this.magic / 100, 1), 0),
+				top:Math.max(291 - 74 * Math.min(ctrl.magic / 100, 1), 0),
 				ease:Power4.easeOut
 			});
-			var emptyTarget = 100 - this.magic;
-			var fullTarget = this.magic;
+			var emptyTarget = 100 - ctrl.magic;
+			var fullTarget = ctrl.magic;
 			TweenMax.to(brValues, 0.5, {
 				empty:emptyTarget,
 				full:fullTarget,
 				ease:Power4.easeOut,
-				onUpdate:this.updateBrText
+				onUpdate:ctrl.updateBrText
 			});
 		}
 
-		this.addElixir = function(elixir) {
+		ctrl.addElixir = function(elixir) {
 			var brMin;
 			var brMax;
 			switch (elixir)
@@ -81,29 +82,29 @@
 					brMax = 0;
 					break;
 			}
-			this.addMagic(randomInt(brMin, brMax));
+			ctrl.addMagic(randomInt(brMin, brMax));
 		};
 
-		this.cancel = function() {
+		ctrl.cancel = function() {
 			TweenMax.killTweensOf(fill);
 			TweenMax.killTweensOf(brEmptyText);
 			TweenMax.killTweensOf(brFullText);
 			TweenMax.killTweensOf(brValues);
-			this.magic = 0;
-			this.calcSuccess();
+			ctrl.magic = 0;
+			ctrl.calcSuccess();
 			fill.css("top", 143);
 			brEmptyText.css("top", 217);
 			brFullText.css("top", 291);
 			brEmptyText.css("display", "initial");
 			brFullText.css("display", "none");
-			scope.brEmpty = 100;
-			scope.brFull = 0;
+			ctrl.brEmpty = 100;
+			ctrl.brFull = 0;
 			brValues.empty = 100;
 			brValues.full = 0;
-			this.selectedElixir = null;
+			ctrl.selectedElixir = null;
 		}
 
-		this.begin = function() {
+		ctrl.begin = function() {
 			
 		}
 	}]);
