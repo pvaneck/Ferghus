5d4faf6d9c6c56de4831dbdd6a58fff8de72a2f4
Christopher Wright <cwright814@gmail.com>
2015-11-10 02:32:57 -0500

    More whitespace?

--- ferghus/assets/angular/app-ferghus.js
+++ ferghus/assets/angular/app-ferghus.js
@@ -1,127 +1,127 @@
 /** Angular App Code */
 (function() {
-	var app = angular.module('ferghus', [ ]);
+    var app = angular.module('ferghus', [ ]);
 
-	app.controller('EnhanceController', ['$scope', '$timeout', function($scope, $timeout) {
-		var scope = $scope;
-		var timeout = $timeout;
-		var ctrl = this;
+    app.controller('EnhanceController', ['$scope', '$timeout', function($scope, $timeout) {
+        var scope = $scope;
+        var timeout = $timeout;
+        var ctrl = this;
 
-		ctrl.selectedItem = null;
-		ctrl.messageOutArray = [];
+        ctrl.selectedItem = null;
+        ctrl.messageOutArray = [];
 
 
-		var popupItems;	
-		var progressBar = $('#progressBar');
-		var messagesToPrint = [
-			'Welcome to the Forge. May the RNG Gods be with you.',
-			'Good luck.'
-		];
+        var popupItems;    
+        var progressBar = $('#progressBar');
+        var messagesToPrint = [
+            'Welcome to the Forge. May the RNG Gods be with you.',
+            'Good luck.'
+        ];
 
-		var enhanceProbabilities = [
-			100, 100, 100, 75, 75, 50, 50, 50, 40, 40, 40, 40, 33.3334, 33.3334, 33.3334
-		];
+        var enhanceProbabilities = [
+            100, 100, 100, 75, 75, 50, 50, 50, 40, 40, 40, 40, 33.3334, 33.3334, 33.3334
+        ];
 
-		function addChatMessage(message) {
-			ctrl.messageOutArray.push(message);
-		}
+        function addChatMessage(message) {
+            ctrl.messageOutArray.push(message);
+        }
 
-		function printMessages() {
-			angular.forEach(messagesToPrint, function(message, index) {
-				$timeout(function() {
-					ctrl.messageOutArray.push(message);
-				}, index * 1500);
-			});
-		}
-		printMessages();
+        function printMessages() {
+            angular.forEach(messagesToPrint, function(message, index) {
+                $timeout(function() {
+                    ctrl.messageOutArray.push(message);
+                }, index * 1500);
+            });
+        }
+        printMessages();
 
-		ctrl.getArray = function(n) {
-			return new Array(n);
-		};
+        ctrl.getArray = function(n) {
+            return new Array(n);
+        };
 
-		ctrl.getItem = function() {
-			popupItems = $('#items').bPopup({
-				speed: 'fast',
-				follow: [false, false],
-				position: [94, 180],
-				opacity: 0.6
-			});
-		}
+        ctrl.getItem = function() {
+            popupItems = $('#items').bPopup({
+                speed: 'fast',
+                follow: [false, false],
+                position: [94, 180],
+                opacity: 0.6
+            });
+        }
 
-		ctrl.selectItem = function(item) {
-			ctrl.selectedItem = item;
-			popupItems.close();
-		}
+        ctrl.selectItem = function(item) {
+            ctrl.selectedItem = item;
+            popupItems.close();
+        }
 
-		ctrl.begin = function() {
-			if (ctrl.selectedItem === null || ctrl.selectedItem >= 15) {
-				return;
-			}
-			if (!muted) {
-				audioEnhance.volume = 0.7;
-				audioEnhance.currentTime = 0;
-				audioEnhance.play();
-			}
-			$('#progress').bPopup({
-				speed: 'fast',
-				followSpeed: 250,
-				opacity: 0.3,
-				modalClose: false,
-				autoClose: 1850,
-				onClose: ctrl.doEnhance
-			});
-			TweenMax.to(progressBar, 2.1, {
-				scaleX:1,
-				ease:Power0.easeOut
-			});
-		}
+        ctrl.begin = function() {
+            if (ctrl.selectedItem === null || ctrl.selectedItem >= 15) {
+                return;
+            }
+            if (!muted) {
+                audioEnhance.volume = 0.7;
+                audioEnhance.currentTime = 0;
+                audioEnhance.play();
+            }
+            $('#progress').bPopup({
+                speed: 'fast',
+                followSpeed: 250,
+                opacity: 0.3,
+                modalClose: false,
+                autoClose: 1850,
+                onClose: ctrl.doEnhance
+            });
+            TweenMax.to(progressBar, 2.1, {
+                scaleX:1,
+                ease:Power0.easeOut
+            });
+        }
 
-		ctrl.doEnhance = function() {
-			var roll = randomFloat(0, 100);
-			roll = Math.round(roll*10000)/10000;
-			var prob = enhanceProbabilities[ctrl.selectedItem];
-			if (roll < prob) {
-				if (prob >= 100)
-					addChatMessage('Enhance successful. (' + prob.toFixed(0) + '% success)');
-				else
-					addChatMessage('Enhance successful. (' + prob.toFixed(0) + '% success, roll: ' + roll + ')');
-				ctrl.selectedItem++;
-			}
-			else {
-				addChatMessage('Enhance failed. (' + prob.toFixed(0) + '% success, roll: ' + roll + ')');
-				switch (ctrl.selectedItem)
-				{
-					case 3:
-					case 4:
-						ctrl.selectedItem--;
-						break;
-					case 5:
-					case 6:
-					case 7:
-						ctrl.selectedItem = 0;
-						break;
-					default:
-						ctrl.selectedItem = null;
-						break;
-				}
-			}
-			scope.$digest();
-			TweenMax.to(progressBar, 0, {scaleX:0});
-		}
-	}]);
+        ctrl.doEnhance = function() {
+            var roll = randomFloat(0, 100);
+            roll = Math.round(roll*10000)/10000;
+            var prob = enhanceProbabilities[ctrl.selectedItem];
+            if (roll < prob) {
+                if (prob >= 100)
+                    addChatMessage('Enhance successful. (' + prob.toFixed(0) + '% success)');
+                else
+                    addChatMessage('Enhance successful. (' + prob.toFixed(0) + '% success, roll: ' + roll + ')');
+                ctrl.selectedItem++;
+            }
+            else {
+                addChatMessage('Enhance failed. (' + prob.toFixed(0) + '% success, roll: ' + roll + ')');
+                switch (ctrl.selectedItem)
+                {
+                    case 3:
+                    case 4:
+                        ctrl.selectedItem--;
+                        break;
+                    case 5:
+                    case 6:
+                    case 7:
+                        ctrl.selectedItem = 0;
+                        break;
+                    default:
+                        ctrl.selectedItem = null;
+                        break;
+                }
+            }
+            scope.$digest();
+            TweenMax.to(progressBar, 0, {scaleX:0});
+        }
+    }]);
 
-	app.directive('scrollBottom', function () {
-		return {
-			scope: {
-				scrollBottom: '='
-			},
-			link: function (scope, element) {
-				scope.$watchCollection('scrollBottom', function (newValue) {
-					if (newValue) {
-						$(element).scrollTop($(element)[0].scrollHeight);
-					}
-				});
-			}
-		}
-	});
+    app.directive('scrollBottom', function () {
+        return {
+            scope: {
+                scrollBottom: '='
+            },
+            link: function (scope, element) {
+                scope.$watchCollection('scrollBottom', function (newValue) {
+                    if (newValue) {
+                        $(element).scrollTop($(element)[0].scrollHeight);
+                    }
+                });
+            }
+        }
+    });
 })();
--- ferghus/assets/js/common.js
+++ ferghus/assets/js/common.js
@@ -1,33 +1,33 @@
 // Helper functions
 var mt = new MersenneTwister();
 function randomInt(min, max) {
-	return Math.floor(mt.random() * (max - min + 1) + min);
+    return Math.floor(mt.random() * (max - min + 1) + min);
 }
 function randomFloat(min, max) {
-	return mt.random() * (max - min) + min;
+    return mt.random() * (max - min) + min;
 }
 
 // Manual .active for buttons (fixes Firefox)
 var lastActive = null;
 $('.canClick').mousedown(function(e) {
-	lastActive = $(this);
-	lastActive.addClass('active');
+    lastActive = $(this);
+    lastActive.addClass('active');
 });
 $('body').mouseup(function(e) {
-	if (lastActive !== null)
-		lastActive.removeClass('active');
+    if (lastActive !== null)
+        lastActive.removeClass('active');
 });
 
 // Prevent text selection
 $('body *').not(':has(.canType)').not('.canType').mousedown(function(e) {
-	e.preventDefault();
+    e.preventDefault();
 });
 
 // Endless sway ease for BG
 function bgSway() {
-	var bg = $('#bg');
-	TweenMax.to(bg, 12, {x:64, ease:Sine.easeInOut});
-	TweenMax.to(bg, 12, {x:-64, ease:Sine.easeInOut, delay: 12, onComplete:bgSway});
+    var bg = $('#bg');
+    TweenMax.to(bg, 12, {x:64, ease:Sine.easeInOut});
+    TweenMax.to(bg, 12, {x:-64, ease:Sine.easeInOut, delay: 12, onComplete:bgSway});
 }
 bgSway();
 
@@ -38,35 +38,35 @@ var mute = $('#mute');
 var muted = false;
 
 function setAudio(file) {
-	var audio = new Audio();
-	if (audio.canPlayType('audio/ogg'))
-		return new Audio('assets/sound/' + file + '.ogg');
-	else
-		return new Audio('assets/sound/' + file + '.mp3');
+    var audio = new Audio();
+    if (audio.canPlayType('audio/ogg'))
+        return new Audio('assets/sound/' + file + '.ogg');
+    else
+        return new Audio('assets/sound/' + file + '.mp3');
 }
 
 mute.click(function () {
-	muted = !muted;
-	if (muted) {
-		$(this).css('opacity', '0.5');
-		audioAmbience.volume = 0;
-	} else {
-		$(this).css('opacity', '1');
-		audioAmbience.volume = ambienceVolume;
-	}
+    muted = !muted;
+    if (muted) {
+        $(this).css('opacity', '0.5');
+        audioAmbience.volume = 0;
+    } else {
+        $(this).css('opacity', '1');
+        audioAmbience.volume = ambienceVolume;
+    }
 });
 
 // On-load logic
 $(document).ready(function () {
-	audioAmbience.volume = ambienceVolume;
-	audioAmbience.play();
+    audioAmbience.volume = ambienceVolume;
+    audioAmbience.play();
 
-	// Gapless audio looping
-	audioAmbience.addEventListener('timeupdate', function() {
-		var buffer = .44;
-		if(this.currentTime > this.duration - buffer) {
-			this.currentTime = 0;
-			this.play();
-		}
-	}, false);
+    // Gapless audio looping
+    audioAmbience.addEventListener('timeupdate', function() {
+        var buffer = .44;
+        if(this.currentTime > this.duration - buffer) {
+            this.currentTime = 0;
+            this.play();
+        }
+    }, false);
 });
