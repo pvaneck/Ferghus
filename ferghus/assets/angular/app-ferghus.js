2cfef6acc8ce21642b4b3c15a5bc9e82e71a3ef9
Christopher Wright <cwright814@gmail.com>
2015-11-09 21:40:48 -0500

    Initial setup for Ferghus

--- ferghus/assets/angular/app-ferghus.js
+++ ferghus/assets/angular/app-ferghus.js
@@ -1,4 +1,23 @@
 /** Angular App Code */
 (function() {
 	var app = angular.module('ferghus', [ ]);
+
+	app.controller('EnchantController', ['$scope', '$timeout', function($scope, $timeout) {
+
+	}]);
+
+	app.directive('scrollBottom', function () {
+        return {
+            scope: {
+                scrollBottom: '='
+            },
+            link: function (scope, element) {
+                scope.$watchCollection('scrollBottom', function (newValue) {
+                    if (newValue) {
+                        $(element).scrollTop($(element)[0].scrollHeight);
+                    }
+                });
+            }
+        }
+    });
 })();
